8d9e720e9bf14ec25c9aae8358cfdd5aac02727d
Jacek <lok7ok@gmail.com>
2021-01-04 15:59:49 +0100

    format and improve (hopfully fix as well) gifs and daydeal, add pycharm files

--- modules/daydeal.py
+++ modules/daydeal.py
@@ -1,98 +1,95 @@
 import discord
-import random
 from discord.ext import commands, tasks
-from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
-from datetime import date, time, datetime
-
-Discord_Bot_Dir = Path("./")
-linksPath = Path(Discord_Bot_Dir/"links/")
+from datetime import datetime
 
 URL = "https://www.daydeal.ch/"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+
 
 async def availableBarCreator(available):
     bar = "["
     if int(available) == 50:
-        bar += ("l"*int(available))+"]"
+        bar += ("l" * int(available)) + "]"
     else:
-        bar += ("l"*int(available))+"||"+("l‏‎‎"*(50-int(available)))+"||]"
-    bar += " ("+str(int(available)*2)+"%)"
+        bar += ("l" * int(available)) + "||" + ("l" * (50 - int(available))) + "||]"
+    bar += " (" + str(int(available) * 2) + "%)"
     return bar
+
 
 class Daydeal(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.page = requests.get(URL)
+        self.soup = BeautifulSoup(self.page.content, 'html.parser')
+        self.channel = None
+        self.mention_role = None
+        self.endTime = datetime.strptime(self.soup.find('div', class_='product-bar__offer-ends').findChild()['data-next-deal'], '%Y-%m-%d %H:%M:%S')
 
     @commands.command()
     @commands.has_permissions(manage_channels=True)
-    async def setupDaydeal(self, ctx, channel : discord.TextChannel, mentionRole : discord.Role):
+    async def setupDaydeal(self, ctx, channel: discord.TextChannel, mention_role: discord.Role):
         self.channel = channel
-        self.mentionRole = mentionRole
-        if self.channel and self.mentionRole:
+        self.mention_role = mention_role
+        if self.channel and self.mention_role:
             await ctx.invoke(self.bot.get_command('deal'))
             await ctx.channel.send("Setup successful.")
             await self.daydeal_task.start(ctx)
 
     @setupDaydeal.error
     async def setupDaydeal_error(self, ctx, error):
-        await ctx.channel.send("Error. Please use command like this: ```,setupDaydeal #channel @role``` Error cause: "+str(error))
+        await ctx.channel.send("Error. Please use command like this: ```,setupDaydeal #channel @role``` Error cause: " + str(error))
 
     @commands.command()
-    async def stopDaydeal(self, ctx):
+    async def stopDaydeal(self):
         await self.daydeal_task.cancel()
 
-    @tasks.loop(seconds=60.0)
+    @tasks.loop(seconds=180.0)
     async def daydeal_task(self, ctx):
-        current_time = datetime.now()
-        endTime = datetime.strptime(soup.find('div', class_='product-bar__offer-ends').findChild()['data-next-deal'], '%Y-%m-%d %H:%M:%S')
-        if(current_time >= endTime):
+        if datetime.now() >= self.endTime:
             await ctx.invoke(self.bot.get_command('deal'))
 
     @commands.command()
     async def deal(self, ctx):
-        page = requests.get(URL)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        product_description = soup.find('section', class_='product-description')
+        self.page = requests.get(URL)
+        self.soup = BeautifulSoup(self.page.content, 'html.parser')
+        self.endTime = datetime.strptime(self.soup.find('div', class_='product-bar__offer-ends').findChild()['data-next-deal'], '%Y-%m-%d %H:%M:%S')
+        product_description = self.soup.find('section', class_='product-description')
         title1 = product_description.find('h1', class_='product-description__title1').text
         title2 = product_description.find('h2', class_='product-description__title2').text
         description_details = product_description.find('ul', class_='product-description__list').findChildren()
-        product_img = soup.find('img', class_='product-img-main-pic')['src']
-        newPrice = soup.find('h2', class_='product-pricing__prices-new-price').text
-        oldPrice = soup.find('span', class_='js-old-price')
-        if oldPrice is not None:
-            oldPrice = oldPrice.text
+        product_img = self.soup.find('img', class_='product-img-main-pic')['src']
+        new_price = self.soup.find('h2', class_='product-pricing__prices-new-price').text
+        old_price = self.soup.find('span', class_='js-old-price')
+        if old_price is not None:
+            old_price = old_price.text
         else:
-            oldPrice = ""
-        available = int(soup.find('strong', class_='product-progress__availability').text.strip('%'))/2
-        availableBar = await availableBarCreator(available)
-        endTime = datetime.strptime(soup.find('div', class_='product-bar__offer-ends').findChild()['data-next-deal'], '%Y-%m-%d %H:%M:%S')
-        endsIn = endTime - datetime.now().replace(microsecond=0)
+            old_price = ""
+        available = int(self.soup.find('strong', class_='product-progress__availability').text.strip('%')) / 2
+        ends_in = self.endTime - datetime.now().replace(microsecond=0)
         description_str = ""
         for element in description_details:
-            description_str += "• "+element.text+"\n"
+            description_str += "• " + element.text + "\n"
 
         embed = discord.Embed(
-            title = title2,
-            description = description_str,
-            url="https://www.daydeal.ch/",
-            colour = discord.Colour.from_rgb(35,180,12)
+            title=title2,
+            description=description_str,
+            url=URL,
+            colour=discord.Colour.from_rgb(35, 180, 12)
         )
         embed.set_thumbnail(url="https://static.daydeal.ch/2.17.10/images/logo-top.png")
         embed.set_image(url=product_img)
-        embed.set_author(name='Todays deal: ' + title1)
-        embed.add_field(name="Price", value="Now: "+newPrice+", Old: "+oldPrice, inline=False)
-        embed.add_field(name="Available", value=availableBar, inline=False)
-        embed.add_field(name="Ends in", value=endsIn, inline=False)
+        embed.set_author(name='Today\'s deal: ' + title1)
+        embed.add_field(name="Price", value="Now: " + new_price + ", Old: " + old_price, inline=False)
+        embed.add_field(name="Available", value=str(await availableBarCreator(available)), inline=False)
+        embed.add_field(name="Ends in", value=str(ends_in), inline=False)
         await self.channel.send(embed=embed)
-        await self.channel.send(self.mentionRole.mention)
+        await self.channel.send(self.mention_role.mention)
 
     @deal.error
     async def deal_error(self, ctx, error):
-        await ctx.channel.send('error:  '+str(error))
+        await ctx.channel.send('error:  ' + str(error))
+
 
 def setup(bot):
     bot.add_cog(Daydeal(bot))
-
--- modules/gif.py
+++ modules/gif.py
@@ -4,75 +4,49 @@ from discord.ext import commands
 from pathlib import Path
 
 Discord_Bot_Dir = Path("./")
-linksPath = Path(Discord_Bot_Dir/"links/")
+linksPath = Path(Discord_Bot_Dir / "links/")
+
 
 async def lineChooser(filename):
     lines = open(linksPath / filename).read().splitlines()
     return random.choice(lines)
+
+
+async def embedSender(self, ctx, member, text, file):
+    member = ctx.author if member is None else member
+    author = self.bot.user if member is ctx.author else ctx.author
+    embed = discord.Embed(
+        title=author.name + text + member.name,
+        colour=discord.Colour.from_rgb(0, 0, 0)
+    )
+    embed.set_image(url=await lineChooser(file))
+    await ctx.channel.send(embed=embed)
+
 
 class Gif(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(brief='Hug someone or yourself')
-    async def hug(self, ctx, member : discord.Member=None):
-        member = ctx.author if member is None else member
-        author = self.bot.user if member is ctx.author else ctx.author
-        embed = discord.Embed(
-            title = author.name + ' hugs ' + member.name + ' 🤗',
-            colour = discord.Colour.from_rgb(0,0,0)
-        )
-        embed.set_image(url=await lineChooser("hug.txt"))
-        await ctx.channel.send(embed=embed)
-        file.close()
+    async def hug(self, ctx, member: discord.Member = None):
+        await embedSender(self, ctx, member, " hugs ", "hug.txt")
 
     @commands.command(brief='Bite someone or yourself')
-    async def bite(self, ctx, member : discord.Member=None):
-        member = ctx.author if member is None else member
-        author = self.bot.user if member is ctx.author else ctx.author
-        embed = discord.Embed(
-            title = str(author.name) + ' bites ' + str(member.name),
-            colour = discord.Colour.from_rgb(0,0,0)
-        )
-        embed.set_image(url=await lineChooser("bite.txt"))
-        await ctx.channel.send(embed=embed)
-        file.close()
-    
+    async def bite(self, ctx, member: discord.Member = None):
+        await embedSender(self, ctx, member, " bites ", "bite.txt")
+
     @commands.command(brief='Cuddle someone or yourself')
-    async def cuddle(self, ctx, member : discord.Member=None):
-        member = ctx.author if member is None else member
-        author = self.bot.user if member is ctx.author else ctx.author
-        embed = discord.Embed(
-            title = str(author.name) + ' cuddles ' + str(member.name) + ' 🤗',
-            colour = discord.Colour.from_rgb(0,0,0)
-        )
-        embed.set_image(url=await lineChooser("cuddle.txt"))
-        await ctx.channel.send(embed=embed)
-        file.close()
+    async def cuddle(self, ctx, member: discord.Member = None):
+        await embedSender(self, ctx, member, " cuddles ", "cuddle.txt")
 
     @commands.command(brief='Kiss someone or yourself')
-    async def kiss(self, ctx, member : discord.Member=None):
-        member = ctx.author if member is None else member
-        author = self.bot.user if member is ctx.author else ctx.author
-        embed = discord.Embed(
-            title = str(author.name) + ' kisses ' + str(member.name) + ' 💋',
-            colour = discord.Colour.from_rgb(0,0,0)
-        )
-        embed.set_image(url=await lineChooser("kiss.txt"))
-        await ctx.channel.send(embed=embed)
-        file.close()
+    async def kiss(self, ctx, member: discord.Member = None):
+        await embedSender(self, ctx, member, " kisses ", "kiss.txt")
 
     @commands.command(brief='Slap someone or yourself')
-    async def slap(self, ctx, member : discord.Member=None):
-        member = ctx.author if member is None else member
-        author = self.bot.user if member is ctx.author else ctx.author
-        embed = discord.Embed(
-            title = str(author.name) + ' slaps ' + str(member.name) + ' 😡',
-            colour = discord.Colour.from_rgb(0,0,0)
-        )
-        embed.set_image(url=await lineChooser("slap.txt"))
-        await ctx.channel.send(embed=embed)
-        file.close()
+    async def slap(self, ctx, member: discord.Member = None):
+        await embedSender(self, ctx, member, " slaps ", "slap.txt")
+
 
 def setup(bot):
     bot.add_cog(Gif(bot))

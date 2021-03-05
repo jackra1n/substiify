1dcc00a4e54fb1413a5d707ef605655a4e458809
Jacek <lok7ok@gmail.com>
2021-03-05 23:05:17 +0100

    fix and improve "epic" command, cleanup code

--- modules/epicGames.py
+++ modules/epicGames.py
@@ -7,7 +7,9 @@ from discord.ext import commands
 
 Discord_Bot_Dir = Path("./")
 linksPath = Path(Discord_Bot_Dir / "links/")
-epicStoreFreeGamesURL = "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=GB&allowCountries=GB,US"
+epicStoreFreeGamesAPI = "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=GB&allowCountries=GB,US"
+epicGamesLogo = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Epic_Games_logo.svg/50px-Epic_Games_logo.svg.png"
+epicStoreFreeGamesURL = "https://www.epicgames.com/store/en-US/free-games"
 
 
 class EpicGames(commands.Cog):
@@ -17,18 +19,26 @@ class EpicGames(commands.Cog):
     @commands.cooldown(3, 30)
     @commands.command()
     async def epic(self, ctx):
-        jsonEpicStore = requests.get(epicStoreFreeGamesURL).json()["data"]["Catalog"]["searchStore"]["elements"]
-        currentGame = jsonEpicStore[0]
+        allGames = requests.get(epicStoreFreeGamesAPI).json()["data"]["Catalog"]["searchStore"]["elements"]
+        
+        currentGame = None
+        for game in allGames:
+            if game["promotions"]["promotionalOffers"]:
+                currentGame = game
+                break
+
         gameTimes = currentGame["promotions"]["promotionalOffers"][0]["promotionalOffers"][0]
         startTime = datetime.strptime(gameTimes["startDate"].split('T')[0], "%Y-%m-%d").strftime('%d %B %Y')
         endTime = datetime.strptime(gameTimes["endDate"].split('T')[0], "%Y-%m-%d").strftime('%d %B %Y')
+        originalPrice = currentGame["price"]["totalPrice"]["fmtPrice"]["originalPrice"]
+        gameCover = currentGame["keyImages"][1]["url"].replace(" ","%20")
 
-        embed = discord.Embed(title=currentGame["title"],description="[Grab your free game here](https://www.epicgames.com/store/en-US/free-games)",colour=0x000000)
-        embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Epic_Games_logo.svg/50px-Epic_Games_logo.svg.png")
+        embed = discord.Embed(title=currentGame["title"],description=f"[Grab your free game here]({epicStoreFreeGamesURL})",colour=0x000000)
+        embed.set_thumbnail(url=f"{epicGamesLogo}")
         embed.add_field(name="Start date", value=startTime, inline=False)
         embed.add_field(name="End date", value=endTime, inline=False)
-        embed.add_field(name="Price", value="~~" + currentGame["price"]["totalPrice"]["fmtPrice"]["originalPrice"] + "~~ ⟶ Free", inline=False)
-        embed.set_image(url=currentGame["keyImages"][1]["url"])
+        embed.add_field(name="Price", value="~~" + originalPrice + "~~ ⟶ Free", inline=False)
+        embed.set_image(url=f"{gameCover}")
 
         await ctx.channel.send(embed=embed)
 
